5e99bab05136932465b663eb0fe8e36cdae4b861
Program-Monkey <827814323@qq.com>
2016-01-11 15:33:43 +0800

    Update app.config.js
    error： tempApp.directive = $compileProvider.register
    the right way is :tempApp.directive = $compileProvider.directive

--- js/app.config.js
+++ js/app.config.js
@@ -7,7 +7,7 @@ var tempApp = angular.module("templateApp",["ui.router","oc.lazyLoad"])
 .config(["$provide","$compileProvider","$controllerProvider","$filterProvider",
                 function($provide,$compileProvider,$controllerProvider,$filterProvider){
                     tempApp.controller = $controllerProvider.register;
-                    tempApp.directive = $compileProvider.register;
+                    tempApp.directive = $compileProvider.directive;
                     tempApp.filter = $filterProvider.register;
                     tempApp.factory = $provide.factory;
                     tempApp.service  =$provide.service;
