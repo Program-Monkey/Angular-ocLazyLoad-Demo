2aa4b36d87ea669bd856d8f1a141ea8f31411517
Program-Monkey <827814323@qq.com>
2016-01-14 10:55:15 +0800

    Update default.js

--- js/controllers/default.js
+++ js/controllers/default.js
@@ -5,7 +5,6 @@
 "use strict"
 tempApp.controller("defaultCtrl",defaultCtrlFn);
 function defaultCtrlFn(){
-    var vm = this;
-    vm.value = "Hello World";
+    this.value = "Hello World";
 };
 }());
